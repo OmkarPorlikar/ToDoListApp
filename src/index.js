1a8a1296277cae9c9b13c5eb01e5ec8e1e137ead
OmkarPorlikar <128311037+OmkarPorlikar@users.noreply.github.com>
2024-04-13 00:06:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './components/ReduxToolkit/store.js';
+import store from './ReduxToolkit/store.js'
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
